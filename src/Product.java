50e77e12443d8a0a6dcbd5b77dab083a54d13c8a
fui96 <kidwelqn@mail.uc.edu>
2025-04-14 17:34:44 -0400

    innit

---
+++ src/Product.java
@@ -0,0 +1,143 @@
+
+public class Product {
+    //Fields
+    private String Name;
+    private String Description;
+    private double Price;
+    private String ID;
+
+    //Constructor
+    /**
+     * Assigns information to product
+     * @param name Name of Product
+     * @param description Product Description
+     * @param price Price of Product
+     * @param ID Product ID
+     */
+    public Product(String ID ,String name, String description, double price) {
+        this.ID = ID;
+        this.Name = name;
+        this.Description = description;
+        this.Price = price;
+
+    }
+    //Getters
+    /** Gets the Name of the product
+     * @return Product Name
+     */
+    public String getName() {
+        return Name;
+    }
+
+    /**
+     * Gets the product Desc
+     * @return Product Description
+     */
+    public String getDescription() {
+        return Description;
+    }
+
+    /**
+     * Gets the price of the product
+     * @return Product price
+     */
+    public double getPrice() {
+        return Price;
+    }
+
+    /**
+     * Gets Product ID
+     * @return ID
+     */
+    public String getID() {
+        return ID;
+    }
+
+    //Setters
+
+    /**
+     * Sets the Name of the product
+     * @param name Product Name
+     */
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    /**
+     * Sets the description
+     * @param description Product Description
+     */
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    /**
+     * Sets the price of the product
+     * @param price Price of Product
+     */
+    public void setPrice(double price) {
+        Price = price;
+    }
+
+    /**
+     * Sets the Product ID
+     * @param ID Product ID
+     */
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+    //Special Methods and Conversions
+
+    /**
+     * Returns the name and description seperated with a comma
+     * @return Name and description
+     */
+    public String NameAndDesc() {
+        return Name + ", " + Description;
+    }
+    /**
+     * Returns a string in CSV format
+     * @return CSV String
+     */
+    public String toCSV() {
+        return String.join("," , ID, Name, Description, Double.toString(Price));
+    }
+
+    /**
+     * Returns a string in a JSON format
+     * @return JSON String
+     */
+    public String toJSON(){
+       return String.format("{,\"ID\":\"%s\",\"Name\":\"%s\",\"Description\":\"%s\",\"Price\":\"%s\"}"
+               , ID, Name, Description, Double.toString(Price));
+    }
+
+    /**
+     * Returns an XML format String
+     * @return XML String
+     */
+    public String toXML(){
+        return String.format("<Product><ID>%s</ID><Name>%s</Name><Description>%s</Description><Price>%s</Price></Product>"
+                ,ID, Name, Description, Double.toString(Price));
+    }
+
+    public String toRAF(){
+        String RAF = formatID() + ", " + formatName() + ", " + formatDescription();
+        return RAF;
+    }
+
+    public String formatID(){
+        return String.format(ID,6);
+    }
+
+    public String formatName(){
+        return String.format(Name,35);
+    }
+
+    public String formatDescription(){
+        return String.format(Description,75);
+    }
+
+
+
+}
---
+++ src/RandProductMaker.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+public class RandProductMaker extends JFrame {
+
+    //Fields
+
+    //Panels
+    JPanel MainPanel, TFPanel, TAPanel;
+    //Buttons
+    JButton ButtonSubmit, ButtonClear;
+    //TextFields
+    JTextField TFName,TFDescription,TFID;
+    //TextArea
+    JTextArea TARecords;
+    //ScrollPane
+    JScrollPane TAScroll;
+    public RandProductMaker() {
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
